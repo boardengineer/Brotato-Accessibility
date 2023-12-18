e3f300436f3d744642eb18cdca27dddfb48f33d2
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-12-18 07:30:50 -0800

    reset camera with middle mouse click

--- Pasha-Accessibility/extensions/global/my_camera.gd
+++ Pasha-Accessibility/extensions/global/my_camera.gd
@@ -8,3 +8,6 @@ func _input(event):
 		if event.button_index == BUTTON_WHEEL_DOWN:
 			zoom.x += 0.01
 			zoom.y += 0.01
+		if event.button_index == BUTTON_MIDDLE:
+			zoom.x = 1.00
+			zoom.y = 1.00
